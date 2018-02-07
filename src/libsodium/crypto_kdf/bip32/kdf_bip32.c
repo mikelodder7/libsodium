e78abaea697542f012e548b7a70ffa6ba1ae4964
Michael Lodder <redmike7@gmail.com>
2018-02-07 11:52:02 -0700

    Adding first code BIP32 functionality
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

---
+++ src/libsodium/crypto_kdf/bip32/kdf_bip32.c
@@ -0,0 +1,14 @@
+#include <errno.h>
+
+#include "crypto_kdf_bip32.h"
+#include "private/common.h"
+
+size_t crypto_kdf_bip32_child_append_bytes(void)
+{
+    return crypto_kdf_bip32_CHILD_APPEND_BYTES;
+}
+
+size_t crypto_kdf_bip32_harden(void)
+{
+    return crypto_kdf_bip32_HARDEN;
+}
